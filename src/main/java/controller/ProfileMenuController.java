d693f7785ee93c8d95b3bc9588eb8b1c7505889f
srmp47 <srmpoor47@gmail.com>
2024-06-06 13:07:57 +0430

    start controller of profile menu

--- src/main/java/controller/ProfileMenuController.java
+++ src/main/java/controller/ProfileMenuController.java
@@ -1,21 +1,63 @@
 package controller;
 
 import model.Result;
+import model.User;
 
 public class ProfileMenuController {
     public Result changeUsername(String username){
-        return null;//TODO: delete this code and write
+        if(User.getLoggedInUser().getUsername().equals(username))
+            return new Result(false,"it's the same as your current username");
+        else if(username.equals(""))
+            return new Result(false,"empty username");
+        else if(User.getUserByUsername(username)!=null)
+            return new Result(false, "there is exist an user with this username");
+        else if (!ErrorController.isNameFormatTrue(username)) {
+            return new Result(false, "wrong username format");
+        }
+        else {
+            User.getLoggedInUser().setUsername(username);
+            return new Result(true, "username changed successfully");
+        }
     }
     public Result changeNickname(String nickname){
-        return null;//TODO: delete this code and write
+        if(User.getLoggedInUser().getNickname().equals(nickname))
+            return new Result(false,"it's the same as your current nickname");
+        else if(nickname.equals(""))
+            return new Result(false,"empty nickname");
+        else{
+            User.getLoggedInUser().setNickname(nickname);
+            return new Result(true,"nickname changed successfully");
+        }
     }
     public Result changeEmail(String email){
-        return null;//TODO: delete this code and write
+        if(User.getLoggedInUser().getEmail().equals(email))
+            return new Result(false,"it's the same as your current email");
+        else if(email.equals(""))
+            return new Result(false,"empty email");
+        else if(!ErrorController.isEmailFormatTrue(email))
+            return new Result(false,"wrong email format");
+        else{
+            User.getLoggedInUser().setEmail(email);
+            return new Result(true,"email changed successfully");
+        }
     }
-    public Result changePassword(String password){
-        return null;//TODO: delete this code and write
+    public Result changePassword(String oldPassword,String newPassword){
+        if(!User.getLoggedInUser().getPassword().equals(oldPassword))
+            return new Result(false,"old password in wrong");
+        if(oldPassword.equals(newPassword))
+            return new Result(false,"it's the same as your current password");
+        else if(newPassword.equals(""))
+            return new Result(false,"empty password");
+        else if(!ErrorController.isPasswordFormatTrue(newPassword))
+            return new Result(false,"wrong password format");
+        else if(ErrorController.isPasswordWeak(newPassword))
+            return new Result(false,"weak password");
+        else{
+            User.getLoggedInUser().setPassword(newPassword);
+            return new Result(true,"password changed successfully");
+        }
     }
     public Result showGamesHistory(int numberOfGames){
-        return null;//TODO: delete this code and write
+        return null;
     }
 }
