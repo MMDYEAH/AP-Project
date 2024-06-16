d3cb77930396cb76e922e367c039004ca16787f2
srmp47 <srmpoor47@gmail.com>
2024-06-16 12:05:51 +0430

    writing apply functions in unit cards

--- src/main/java/model/MoralBooster.java
+++ src/main/java/model/MoralBooster.java
@@ -1,12 +1,15 @@
 package model;
 
-public class MoralBooster extends UnitCard{
+public class MoralBooster extends UnitCard {
     public MoralBooster(String name, int primitiveNumberOfCards, int power, boolean isLegendary) {
         super(name, primitiveNumberOfCards, power, isLegendary);
     }
 
     @Override
     public void apply() {
-
+        for (Card card : unit.cards) {
+            UnitCard unitCard = (UnitCard) card;
+            if (!unitCard.isLegendary && !unitCard.equals(this)) unitCard.setPower(unitCard.getPower() + 1);
+        }
     }
 }
