e37a9586543a71d3af9dbf79d7a8f04b35babc1d
AmirMasoud Ebrahimhi <amirm.eb04@gmail.com>
2024-06-25 14:28:21 +0330

    cards upload 90% complete

---
+++ src/main/java/model/UnitMardroeme.java
@@ -0,0 +1,13 @@
+package model;
+
+public class UnitMardroeme extends UnitCard{
+
+    public UnitMardroeme(String name, int power, boolean isLegendary, String path) {
+        super(name, power, isLegendary, path);
+    }
+
+    @Override
+    public void apply() {
+
+    }
+}
