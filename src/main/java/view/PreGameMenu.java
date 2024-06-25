34f841b0e696e902cebb798f34fef9a85806fad1
srmp47 <srmpoor47@gmail.com>
2024-06-26 01:08:09 +0430

    a

--- src/main/java/view/PreGameMenu.java
+++ src/main/java/view/PreGameMenu.java
@@ -1,13 +1,52 @@
 package view;
 
+import controller.PreGameMenuController;
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
+import model.App;
 
-import java.util.Scanner;
+import java.io.IOException;
 
 public class PreGameMenu extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
 
+    @FXML
+    public ScrollPane scrollPane;
+    PreGameMenuController controller = new PreGameMenuController(this);
+
+    @FXML
+    public TilePane tilePane;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        App.setStage(stage);
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginMenu.class.getResource("/PreGame.fxml"));
+        Pane pane = fxmlLoader.load();
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+
+
+        Image image = new Image(getClass().getResource("/pics/neutral/Decoy.jpg").toExternalForm());
+        ImageView imageView = new ImageView(image);
+        scrollPane = (ScrollPane) scene.lookup("#scrollPane");
+        tilePane = (TilePane) scrollPane.getContent().lookup("#tilePane");
+        tilePane.getChildren().add(imageView);
+        stage.setFullScreen(true);
+        stage.setResizable(false);
+        stage.show();
     }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
+
