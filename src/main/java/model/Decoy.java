dc3c9e8fe81c3117b5d36c459d2b2b031677529c
srmp47 <srmpoor47@gmail.com>
2024-06-27 17:35:58 +0430

    writing apply function in a few cards

--- src/main/java/model/Decoy.java
+++ src/main/java/model/Decoy.java
@@ -4,9 +4,13 @@ public class Decoy extends Card{
     public Decoy(String name, String path) {
         super(name,path);
     }
-
+    private Card cardShouldBeReplaced;
+    public void setCardShouldBeReplaced(Card card){
+        cardShouldBeReplaced = card;
+    }
     @Override
     public void apply() {
-
+       cardShouldBeReplaced.unit.removeCardFromUnit(cardShouldBeReplaced);
+       Game.currentGame.currentUser.getPlayBoard().getHandUnit().addCardToUnit(cardShouldBeReplaced);
     }
 }
--- src/main/java/model/TightBound.java
+++ src/main/java/model/TightBound.java
@@ -7,6 +7,13 @@ public class TightBound extends UnitCard{
 
     @Override
     public void apply() {
-
+        int numberOfSimilarCards = 0;
+         for(Card card : this.getUnit().cards){
+             UnitCard unitCard = (UnitCard) card;
+             if(unitCard.name.equals(this.name) && !unitCard.equals(this))
+                 unitCard.setPower(unitCard.getPower()+this.power);
+             numberOfSimilarCards++;
+         }
+         this.setPower(this.getPower()*(numberOfSimilarCards+1));
     }
 }
