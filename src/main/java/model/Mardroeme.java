eab871edfa0b3cb0ff769177e0f96b505ee37c81
mmd_yeah <mohamadmahmodieh@gmail.com>
2024-07-04 20:59:43 +0330

    Mardroene, UnitMardroeme and Medic classes

--- src/main/java/model/Mardroeme.java
+++ src/main/java/model/Mardroeme.java
@@ -1,16 +1,32 @@
 package model;
 
-public class Mardroeme extends Card{
+public class Mardroeme extends Card {
     public Mardroeme(String name, String path) {
         super(name, path);
     }
 
     @Override
     public void apply() {
-
+        for (Card card : unit.cards) {
+            if (card instanceof Berserker) {
+                transformToMoralBooster((Berserker) card);
+            }
+        }
     }
+
+    private void transformToMoralBooster(Berserker berserker) {
+        MoralBooster moralBooster = new MoralBooster("Vidkaarl", berserker.getPower(), berserker.isLegendary(), getClass().getResource("/pics/skellige/Vildkaarl.jpg").toExternalForm());
+
+        berserker.unit.removeCardFromUnit(berserker);
+        berserker.unit.addCardToUnit(moralBooster);
+//        Game.currentGame.currentUser.getPlayBoard().getDiscardPileUnit().addCardToUnit(berserker);
+
+        moralBooster.unit = berserker.unit;
+        moralBooster.apply();
+    }
+
     @Override
     public String toJson() {
-        return super.toJson()+"(type<Mardroeme>)}";
+        return super.toJson() + "(type<Mardroeme>)}";
     }
 }
