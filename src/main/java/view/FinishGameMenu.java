34ee0b0e312f806da934a9bed53eaf86b080f3a7
AmirMasoud Ebrahimhi <amirm.eb04@gmail.com>
2024-07-06 19:34:17 +0330

    finishing game and some repair

--- src/main/java/view/FinishGameMenu.java
+++ src/main/java/view/FinishGameMenu.java
@@ -1,11 +1,13 @@
 package view;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class FinishGameMenu extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        
     }
 }
